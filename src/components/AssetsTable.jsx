a6d8735a9ca7f9cdb5eb4fabe54f7cc7c9aff158
tryall1 <genial712@gmail.com>
2024-08-29 18:26:57 +0200

    added the  table

--- src/components/AssetsTable.jsx
+++ src/components/AssetsTable.jsx
@@ -1,3 +1,50 @@
+import { Table } from 'antd';
+import { useCrypto } from '../context/crypto-context';
+const columns = [
+  {
+    title: 'Name',
+    dataIndex: 'name',
+    showSorterTooltip: {
+      target: 'full-header',
+    },
+    onFilter: (value, record) => record.name.indexOf(value) === 0,
+    sorter: (a, b) => a.name.length - b.name.length,
+    sortDirections: ['descend'],
+  },
+  {
+    title: 'Price, $',
+    dataIndex: 'price',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.price - b.price,
+  },
+  {
+    title: 'Amount',
+    dataIndex: 'amount',
+    defaultSortOrder: 'descend',
+    sorter: (a, b) => a.amount - b.amount,
+  },
+];
+
+
 export default function AssetsTable() {
-    return <div>111</div>
+    const {assets} = useCrypto()
+
+    const data = assets.map((a) => ({
+        key: a.id,
+        name: a.name,
+        price: a.price,
+        amount: a.amount,
+    }))
+    const onChange = (pagination, filters, sorter, extra) => {
+        console.log('params', pagination, filters, sorter, extra);
+      };
+    return <Table
+    pagination={false}
+    columns={columns}
+    dataSource={data}
+    onChange={onChange}
+    showSorterTooltip={{
+      target: 'sorter-icon',
+    }}
+  /> 
 }
